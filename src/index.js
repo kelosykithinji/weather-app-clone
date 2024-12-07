2f7211652106c5a176f30b3babd6cfed240bb639
kelosy kithinji <kelosykithinji@gmail.com>
2024-12-07 14:22:11 +0400

    updated wind, speed and description

--- src/index.js
+++ src/index.js
@@ -3,7 +3,26 @@ function displayTemp(response){
     console.log(response);
     let currentTemp = Math.round(response.data.temperature.current);
     let tempElement = document.querySelector("#temp");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeed = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let weekdayElement = document.querySelector("#week-day");
+    let descriptionElement = document.querySelector("#description");
+
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeed.innerHTML = `${response.data.wind.speed} km/h`;
     tempElement.innerHTML = `${currentTemp}`;
+    descriptionElement.innerHTML= response.data.condition.description;
+   // descriptionElement.innerHTML = 
+    
+}
+function changeDateAndDay(date){
+    let weekDays = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let now= newDate();
+    let hour = now.gethours();
+    let minute = now.getminutes();
+
+
 }
 function displayCity(city){
     event.preventDefault();
